e36753b939b9e59cd37b9dda616861af31c00e04
rezackacz <44003501+rezackacz@users.noreply.github.com>
2024-01-16 19:28:30 +0000

    make it look ~~better~~ **more readable**

--- HallwayInfoPanelGMH/HTMLRenderer.cs
+++ HallwayInfoPanelGMH/HTMLRenderer.cs
@@ -8,7 +8,7 @@ namespace HallwayInfoPanelGMH {
   class HTMLRenderer {
 
     private const string htmlBeginning = "<!DOCTYPE html><html><head><title>GMH Infopanel</title><meta charset=\"UTF-16\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><style>";
-    private const string CSS = "body { font-family: Segoe UI, Arial, sans-serif; font-size: 3.5vw;} .table-container { display: flex; flex-direction: column; align-items: center; margin-top: 5px;} .table-row { display: flex; justify-content: space-between; width: 100%; border-bottom: 1px solid #ccc; padding: 10px 0;} .title { text-align:center; font-weight:bold}";
+    private const string CSS = "body { font-family: monospace; font-size: 3.5vw;} .table-container { display: flex; flex-direction: column; align-items: center; margin-top: 5px;} .table-row { display: flex; justify-content: space-between; width: 100%; border-bottom: 1px solid #ccc; padding: 10px 0;} .title { text-align:center; font-weight:bold}";
     private const string htmlMiddle = "</style></head><body>";
     private string body;
     private const string htmlEnding = "</body></html>";
